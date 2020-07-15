a2b87b4dca6f8887c2b571b13856d8d38e3e6b70
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-07-15 15:27:50 +0300

    Add multiplication scripts

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var multiply = function(first, second,third){
+    return first*second,third;
+};
+var numb1 = parseInt(prompt("Please enter a number"));
+var numb2 = parseInt(prompt("Please enter a second number"));
+var numb3 = parseInt(prompt("Enter the third number"));
+var multiply = numb2 *numb1*numb3;
+ alert(multiply);
